5266dc859e3f9c764e25d2b19c2bca29ea73df28
Renaanprofissional <renaan.profissional@gmail.com>
2026-01-26 19:59:12 -0300

    fix: validation with yup

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -6,17 +6,31 @@
    delete -> remover dados
 */
 import { v4 } from 'uuid';
+import * as Yup from 'yup';
 import User from '../models/User.js';
 
 class UserController {
   async store(request, response) {
-    console.log('REQUEST BODY:', request.body);
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().min(6).required(),
+      admin: Yup.boolean(),
+    });
+
+    try {
+      schema.validateSync(request.body, { abortEarly: false, strict: true });
+    } catch (err) {
+      console.log(err);
+      return response.status(400).json({error: err.errors})
+    }
+
     const { name, email, password_hash, admin } = request.body;
 
     const existingUser = await User.findOne({ where: { email } });
 
     if (existingUser) {
-      return response.status(400).json({ message: 'Este e-mail já está sendo usado.' });
+      return response.status(400).json({ message: 'Email already taken!' });
     }
 
     const user = await User.create({
