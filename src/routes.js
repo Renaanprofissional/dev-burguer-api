e037e83b181ae68a6690a63b1929d583b7dee055
Renaanprofissional <renaan.profissional@gmail.com>
2026-01-27 20:23:04 -0300

    fix: user login

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+
+import SessionController from './app/controller/SessionController.js';
 import UserController from './app/controller/UserController.js';
+
+
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store)
 
 export default routes;
 
