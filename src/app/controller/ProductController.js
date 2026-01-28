e2c6c0edf70f9437e6d31d395d7641d9a913af2a
Renaanprofissional <renaan.profissional@gmail.com>
2026-01-28 19:43:40 -0300

    fix: saving image to database

--- src/app/controller/ProductController.js
+++ src/app/controller/ProductController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import Product from '../models/Product.js';
 
 class ProductController {
     async store(request, response) {
@@ -14,9 +15,26 @@ class ProductController {
       console.log(err);
       return response.status(400).json({ error: err.errors });
     }
+
+    const {name, price, category} = request.body
+    const { filename } = request.file
+
+    const newProduct = await Product.create({
+      name,
+      price,
+      category,
+      path: filename
+    })
         
-    return response.status(201).json({ ok: true });
+    return response.status(201).json(newProduct);
     }
+
+    async index(_request, response) {
+    const products = await Product.findAll();
+    return response.status(200).json(products);
+  }
 }
+
+
 
 export default new ProductController();
