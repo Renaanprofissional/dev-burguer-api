24d4ef3b18cd388ef764a6c7ffa2a17269c8a398
Renaanprofissional <renaan.profissional@gmail.com>
2026-01-28 19:12:59 -0300

    fix: upload image and add multer

--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,21 @@
 import { Router } from 'express';
-
+import multer from 'multer';
 import ProductController from './app/controller/ProductController.js';
 import SessionController from './app/controller/SessionController.js';
 import UserController from './app/controller/UserController.js';
+import multerConfig from './config/multer.cjs';
 
 const routes = new Router();
 
+const upload = multer(multerConfig);
+
 routes.post('/users', UserController.store);
-routes.post('/session', SessionController.store)
-routes.post('/products', ProductController.store);
+routes.post('/session', SessionController.store);
+routes.post('/products', upload.single('file'), ProductController.store);
 
 export default routes;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Metodos HTTP -> 
+// Metodos HTTP ->
 /* 
 POST -> criar
 PUT/PATCH -> atualizar, patch atualiza somente um campo
