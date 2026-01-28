afd16cf75efeecdcd793bcb68ba08d4a38b8b6c5
Renaanprofissional <renaan.profissional@gmail.com>
2026-01-28 18:18:17 -0300

    fix: validation product

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 
+import ProductController from './app/controller/ProductController.js';
 import SessionController from './app/controller/SessionController.js';
 import UserController from './app/controller/UserController.js';
-
-
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store)
+routes.post('/products', ProductController.store);
 
 export default routes;
 
