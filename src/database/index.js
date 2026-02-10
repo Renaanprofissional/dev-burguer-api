38db5bd08ec214cd55ec90ab0565ea52c0734859
Renaanprofissional <renaan.profissional@gmail.com>
2026-02-10 18:52:21 -0300

    chore: config mongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,6 @@
+import mongoose from 'mongoose';
 import { Sequelize } from 'sequelize';
+
 import Category from '../app/models/Category.js';
 import Product from '../app/models/Product.js';
 import User from '../app/models/User.js';
@@ -9,16 +11,23 @@ const models = [User, Product, Category];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
     models
-    .map((model) => model.init(this.connection))
-    .map(
-      (model) => model.associate && model.associate(this.connection.models),
-    )
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models),
+      );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/devburguer',
+    );
   }
 }
 
-export default new Database();
+export default new Database();S
